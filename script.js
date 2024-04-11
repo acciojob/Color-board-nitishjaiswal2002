2b0a20bc07510bc18105bd61c65be13d2782e8cb
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-11 20:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.querySelectorAll('.square').forEach(square=>{
+	square.addEventListener('mouseover', ()=>{
+		square.style.backgroundColor='#ffcc00';
+	});
+	square.addEventListner('mouseout', () =>{
+		setTimeout(()=>{
+			sqaure.style.backgroundColor='#ccc';
+		}, 1000);
+	});
+})
